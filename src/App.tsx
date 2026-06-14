2962e030ac7806d5c27d85dce5c001c1c2f18264
YZYL\32767 <3276730596@qq.com>
2026-06-14 22:59:56 +0800

    Optimize: remove heavy CSS effects, change video preload to none

--- src/App.tsx
+++ src/App.tsx
@@ -14,23 +14,9 @@ export default function App() {
     return () => window.removeEventListener('scroll', handleScroll)
   }, [])
 
-  useEffect(() => {
-    const handleMouseMove = (e: MouseEvent) => {
-      const x = (e.clientX / window.innerWidth) * 100
-      const y = (e.clientY / window.innerHeight) * 100
-      document.documentElement.style.setProperty('--mouse-x', x + '%')
-      document.documentElement.style.setProperty('--mouse-y', y + '%')
-    }
-    window.addEventListener('mousemove', handleMouseMove)
-    return () => window.removeEventListener('mousemove', handleMouseMove)
-  }, [])
 
  return (
    <div className="relative bg-black text-white overflow-x-hidden">
-      {/* Noise texture overlay */}
-      <div className="noise-overlay" />
-      {/* Cursor glow */}
-      <div className="cursor-glow" />
       <Navbar scrolled={scrolled} />
       <section id="hero"><Hero /></section>
       <section id="about"><About /></section>
@@ -39,3 +25,4 @@ export default function App() {
     </div>
   )
 }
+
